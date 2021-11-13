312608d85f182ea96b5cc243a6c6b54202bdd4f7
Sergey <livegrafics@rambler.ru>
2021-11-13 11:09:37 +0300

    Correction

--- src/main/java/HomeWork3Part2.java
+++ src/main/java/HomeWork3Part2.java
@@ -1,5 +1,26 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 public class HomeWork3Part2 {
-    public static void main(String[] args) {
+    public static void main (String args[]) {
+        ArrayList <PhoneBook> phonebook = new ArrayList<>();
+        add(phonebook,"Иванов", "9101002030");
+        add(phonebook,"Петров", "9203004050");
+        add(phonebook,"Сидоров", "9304005060");
+        add(phonebook,"Князев", "9405006070");
+        add(phonebook,"Романов", "9506007080");
+        for (int i=0;i< phonebook.size();i++){
+            String s1=phonebook.get(i).family;
+            String s2=phonebook.get(i).number;
+            System.out.println(s1 + s2);
+        }
 
     }
+
+    public static void add(ArrayList ar, String family, String phone){
+        PhoneBook record = new PhoneBook(family, phone);
+        ar.add(record);
+    }
+
 }
